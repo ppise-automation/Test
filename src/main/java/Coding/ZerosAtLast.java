5758d498e6d211fdb72fc590c520f5454ae14d8c
Parth <parthspise@gmail.com>
2025-07-20 13:16:58 +0530

    added enhanced for loop

--- src/main/java/Coding/ZerosAtLast.java
+++ src/main/java/Coding/ZerosAtLast.java
@@ -8,9 +8,9 @@ public class ZerosAtLast {
         int j = 0;
 
 
-        for(int i=0; i< a.length ; i++){
-            if(a[i] != 0){
-                temp[j++] = a[i];
+        for(int h : a){
+            if(h != 0){
+                temp[j++] = h;
             }
         }
 
